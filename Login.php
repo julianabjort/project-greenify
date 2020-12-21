540f423c96e027a3c49167b8d23f34436e42131e
Your Name <julianabjortiv@gmail.com>
2020-12-21 13:16:09 +0000

    padding logo

---
+++ Login.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Greenify</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <header class="header" id="Header2">
+        <div class="Logo" id="Logo2">
+            <a class="Logo" href="HomePage.html"><img src="images/logoGreenify.png" width="60%"></a>
+        </div>
+        <div class="NavigationBar" id="NB2">
+            <nav>
+                <ul class="MenuTop">
+                    <li><a href="Redeem.html">About</a></li>
+                    <li><a href="User.html">Login</a></li>
+                </ul>
+            </nav>
+        </div>
+    </header>
+    <main class="SignUp">
+        <div class="SignUp">
+            <h1>Sign Up</h1>
+            <div class="SignUpBox">
+                <div>
+                    <form action="/action_page.php">
+                        <div class="container">
+                            <input type="text" placeholder="Full name" name="name" required>
+                            <input type="text" placeholder="Email" name="email" required>
+                            <input type="password" placeholder="Password" name="psw" required>
+                            <input type="password" placeholder="Repeat Password" name="psw-repeat" required>
+                            <div class="clearfix">
+                                <button type="submit" class="signupbtn">GET STARTED</button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+                <div>
+                    
+                </div>
+            </div>
+        </div>
+    </main>
+    <footer>
+
+    </footer>
+</body>
+</html>
